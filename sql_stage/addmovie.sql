2b121cfdb6ce1f2e705b43842a21bba97870d27a
matthcol <matthias.colin@gmail.com>
2021-05-05 13:46:55 +0200

    script add a lot of movies

---
+++ sql_stage/addmovie.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE procedure addmovies(
+	pcount number,
+	ptitle varchar, 
+	pyearmin number DEFAULT 1888,
+	pyearmax integer DEFAULT 3000) 
+as 
+	vyearoffset number;
+	vyeardelta number;
+BEGIN
+	vyeardelta := pyearmin - pyearmax;
+	vyearoffset := 0;
+	FOR i IN 1 .. pcount LOOP
+		vyearoffset := mod((vyearoffset + 1), vyeardelta);
+		insert into movies (title, year) values (ptitle || '_' || i, pyearmin + vyearoffset);
+	END LOOP;
+END;
+/
+
+-- call addmovies(100, 'Alien');
+-- select * from movies where title like 'Alien%';
+
+
+select 12 mod 5 from dual;
