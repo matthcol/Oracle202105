9742ea23f189eb2bca97179617e5945f5b688850
matthcol <matthias.colin@gmail.com>
2021-05-07 13:39:03 +0200

    install instance

---
+++ install/createDB.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+dbca -silent -createDatabase                                                   \
+     -templateName General_Purpose.dbc                                         \
+     -gdbname ${ORACLE_SID} -sid  ${ORACLE_SID} -responseFile NO_VALUE         \
+     -characterSet AL32UTF8                                                    \
+     -sysPassword password                                                 \
+     -systemPassword password                                              \
+     -createAsContainerDatabase false                                           \
+     -databaseType MULTIPURPOSE                                                \
+     -memoryMgmtType auto_sga                                                  \
+     -totalMemory 2000                                                         \
+     -storageType FS                                                           \
+     -datafileDestination "${DATA_DIR}"                                        \
+     -redoLogFileSize 50                                                       \
+     -emConfiguration NONE                                                     \
+     -ignorePreReqs
